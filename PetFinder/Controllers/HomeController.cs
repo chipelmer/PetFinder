84ce64e325b52aea3c887d9019ee287484210e75
Chip Elmer <chip.elmer@gmail.com>
2018-05-22 09:39:33 -0500

    Some basic site information added

--- PetFinder/Controllers/HomeController.cs
+++ PetFinder/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace PetFinder.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Connecting lost pets and owners.";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Don't call us, we'll call you.";
 
             return View();
         }
