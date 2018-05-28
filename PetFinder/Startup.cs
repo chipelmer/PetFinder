59e9686034ed53fc4b69fbfd7d4726367261323d
Francesca Marciani <fmarciani@gmail.com>
2018-05-28 18:32:25 -0300

    Added code to Startup to connect to database.

--- PetFinder/Startup.cs
+++ PetFinder/Startup.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MySql.Data.MySqlClient;
+using Finder.Shared;
+using Finder.Repository;
 
 namespace PetFinder
 {
@@ -21,7 +25,13 @@ namespace PetFinder
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connString = Configuration.GetConnectionString("DefaultConnection");
+
             services.AddMvc();
+
+            services.AddTransient<IPetRepo, PetRepository>();
+
+            services.AddScoped<IDbConnection>(_ => new MySqlConnection(connString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
